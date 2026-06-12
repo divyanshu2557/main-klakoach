4cac84579b67e58c6c0009b1b6f1d4e97bb5484b
divyanshu2557 <divyanshu2557@gmail.com>
2026-06-12 07:22:52 +0530

    chore: cleanup project and update gitignore

--- get_order.js
+++
@@ -1,4 +0,0 @@
-import Database from "better-sqlite3";
-const db = new Database("klakoach.db");
-const order = db.prepare("SELECT id FROM orders LIMIT 1").get();
-console.log("ORDER ID:", order?.id);
